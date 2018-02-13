92d6e731aa9f0d0961d30be74f3c542386eb3d85
Jeffrey2000 <jeffltd@outlook.com>
2018-02-13 16:31:27 +0000

    Learning about main menu bar and different functions seems a bit redundant; making me realise why I prefer woirking in back end

--- window.py
+++ window.py
@@ -8,16 +8,36 @@ class mainFrame(QtGui.QMainWindow):
         self.setGeometry(50,50,500,500);
         self.setWindowTitle("Jeffrey's Application")
         self.setWindowIcon(QtGui.QIcon('jlogo.png'));
+
+
+        endApplication = QtGui.QAction("&Quit Application", self);
+        endApplication.setShortcut("Ctrl+X");
+        endApplication.setStatusTip("Leave the application");
+        endApplication.triggered.connect(self.close_application)
+
+        printToConsole = QtGui.QAction("&Print to console", self);
+        printToConsole.triggered.connect(self.printToConsole);
+        
+        
         self.buttonQuit();
+        self.statusBar();
+
+        
+        #Creates menu bar
+        mainMenu = self.menuBar(); #We make this object because we want to modify it
+        fileMenu = mainMenu.addMenu("&File") #Adds sub menu to large menubar
+        fileMenu = mainMenu.addMenu("&Stupid random function")
+        fileMenu.addAction(endApplication); #Application function created beforheand added to the menu
+        fileMenu.addAction(printToConsole);
 
 
     def buttonQuit(self):
         quitButton = QtGui.QPushButton("Quit",self); 
       
-        quitButton.clicked.connect(self.button_resize);#Takes reference to function
+        quitButton.clicked.connect(self.button_resize);
 
 
-        #sizeHint() and minimumSizeHint() return suggested size and minimum suggested size respectively
+       
         quitButton.resize(quitButton.sizeHint())
         quitButton.move(100,100);
         self.show();
@@ -30,6 +50,13 @@ class mainFrame(QtGui.QMainWindow):
 
     def button_resize(self):
         self.setWindowTitle("ZenithList")
+
+    def printToConsole(self):
+        from random import randint
+        for i in range(0,8):
+            print(i*randint(0,8));
+            
+    
 
     
 
