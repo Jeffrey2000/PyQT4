220e81a91790add9f964a010f5b8077e2225d461
Jeffrey2000 <jeffltd@outlook.com>
2018-02-13 16:08:30 +0000

    Learning about binding custom funtions to a button and about different built in PyQt button functions such as sizeHint()

--- window.py
+++ window.py
@@ -1,31 +1,35 @@
 import sys
-from PyQt4 import QtGui,QtCore #Module needed to add button event handling
-
-#Python OOP for GUI, makes sense for anything that may interact with it in a dynamic way
+from PyQt4 import QtGui,QtCore 
 
 class mainFrame(QtGui.QMainWindow):
 
-    def __init__(self): ##Core application stuff, template for the rest of GUI
+    def __init__(self):
         super(mainFrame, self).__init__();
         self.setGeometry(50,50,500,500);
         self.setWindowTitle("Jeffrey's Application")
         self.setWindowIcon(QtGui.QIcon('jlogo.png'));
         self.buttonQuit();
 
-    ##Adding a button to the window
-    def buttonQuit(self):
-        quitButton = QtGui.QPushButton("Quit",self); ##Takes parameter of name and parent calss
-        #Exits current instance of QCore appliation
-        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit);
 
-        #Resizing button and moving button
-        quitButton.resize(100,50)
+    def buttonQuit(self):
+        quitButton = QtGui.QPushButton("Quit",self); 
+      
+        quitButton.clicked.connect(self.button_resize);#Takes reference to function
+
+
+        #sizeHint() and minimumSizeHint() return suggested size and minimum suggested size respectively
+        quitButton.resize(quitButton.sizeHint())
         quitButton.move(100,100);
         self.show();
 
         
 
-        
+    def close_application(self):
+        print("Application closed");
+        sys.exit();
+
+    def button_resize(self):
+        self.setWindowTitle("ZenithList")
 
     
 
